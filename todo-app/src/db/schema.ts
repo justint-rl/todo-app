4de655978b565c34c2272e4b46ccc890041a0bac
justint-rl <justint1004@gmail.com>
2025-10-25 05:15:04 -0400

    Rename variables

--- todo-app/src/db/schema.ts
+++ todo-app/src/db/schema.ts
@@ -1,15 +1,15 @@
 import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
+export const userPgTable = pgTable('user', {
   id: serial('id').primaryKey(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
 
-export type User = typeof user.$inferSelect
-export type NewUser = typeof user.$inferInsert
+export type User = typeof userPgTable.$inferSelect
+export type NewUser = typeof userPgTable.$inferInsert
 
-export const todo = pgTable('todo', {
+export const todoPgTable = pgTable('todo', {
   id: serial('id').primaryKey(),
   userId: serial('user_id').notNull(),
   title: text('title').notNull(),
@@ -19,5 +19,5 @@ export const todo = pgTable('todo', {
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
 
-export type Todo = typeof todo.$inferSelect;
-export type NewTodo = typeof todo.$inferInsert;
+export type Todo = typeof todoPgTable.$inferSelect;
+export type NewTodo = typeof todoPgTable.$inferInsert;
